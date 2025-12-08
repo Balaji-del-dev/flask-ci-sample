acb08326e6ec67502d93f11db8cf5ddbfef1dce8
balamurugan-commits <balamurugan@indusvision.ai>
2025-12-08 21:26:56 +0530

    manoji fixes

--- django_ci/dja_app/tests.py
+++ django_ci/dja_app/tests.py
@@ -1,13 +1,4 @@
 from django.test import TestCase
-from .utils import add_numbers
 
-class CalculationTest(TestCase):
 
-    def test_add_numbers(self):
-        result = add_numbers(2, 3)
-        self.assertEqual(result, 6)
-
-    def test_add_negative_numbers(self):
-        result = add_numbers(-1, -4)
-        self.assertEqual(result, -5)
-
+print(f"Hi this is Manoji fucker")
